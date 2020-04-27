c878d89a42bbb8a7cde6af645367fcd3c2ce4206
ce.chai <ce.chai@7x-networks.com>
2020-04-27 10:03:50 +0800

    Future FutureTask

---
+++ demo/src/main/java/org/ccit/com/FutureDemo.java
@@ -0,0 +1,88 @@
+package org.ccit.com;
+
+import java.util.concurrent.*;
+
+public class FutureDemo {
+
+    /**
+     * Future 相关方法
+     * cancel();取消任务
+     * isCancelled();判断任务是否已取消
+     * isDone();判断任务是否已结束
+     * get();获得任务执行的结果 阻塞式,如果被调用的时候,任务还没有执行完,那么调用get()方法的线程会被阻塞
+     * get(long,TimeUnit);获得任务执行的结果,支持超时机制
+     * ThreadPoolExecutor 三种 submit()
+     * 1.Future<?> submit(Runnable task) 提交Runnable任务.这个方法的参数是一个Runnable接口,Runnable接口的run()是没有返回值的.这个方法返回的Future仅可以用来断言任务已经结束了.类似Thread.join()
+     * 2.<T> Future<T> submit(Runnable task, T result) 提交Runnable任务及结果引用.
+     * 3.<T> Future<T> submit(Callable<T> task) 提交Callable任务.call()是有返回值的,返回的Future对象通过调用其get()获取任务的执行结果
+     */
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        boilingWaterTea();
+    }
+
+    //利用FutureTask很容易获取子线程的执行结果
+    static void futureTaskDemo() throws ExecutionException, InterruptedException {
+        FutureTask<Integer> futureTask = new FutureTask<>(() -> 1 + 2);
+        ExecutorService es = Executors.newCachedThreadPool();
+        es.submit(futureTask);
+        System.out.println(futureTask.get());
+    }
+
+    static void boilingWaterTea() throws ExecutionException, InterruptedException {
+        FutureTask<String> ft2 = new FutureTask<>(new T2Task());
+        FutureTask<String> ft1 = new FutureTask<>(new T1Task(ft2));
+
+        Thread t1 = new Thread(ft1);
+        t1.start();
+
+        Thread t2 = new Thread(ft2);
+        t2.start();
+
+        System.out.println(ft1.get());
+    }
+
+}
+
+//需要执行洗水壶,烧开水,泡茶
+class T1Task implements Callable<String> {
+
+    FutureTask<String> ft2;
+
+    T1Task(FutureTask<String> ft2) {
+        this.ft2 = ft2;
+    }
+
+    @Override
+    public String call() throws Exception {
+        System.out.println("T1:洗水壶");
+        TimeUnit.SECONDS.sleep(1);
+
+        System.out.println("T1:烧开水");
+        TimeUnit.SECONDS.sleep(2);
+
+        //拿茶叶
+        String tf = ft2.get();
+        System.out.println("T1:拿到茶叶:" + tf);
+
+        System.out.println("T1:泡茶");
+
+        return "上茶" + tf;
+    }
+}
+
+//洗茶壶,洗茶杯,拿茶叶
+class T2Task implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        System.out.println("T2:洗茶壶");
+        TimeUnit.SECONDS.sleep(1);
+
+        System.out.println("T2:洗茶杯");
+        TimeUnit.SECONDS.sleep(2);
+
+        System.out.println("T2:拿茶叶");
+        TimeUnit.SECONDS.sleep(1);
+        return "龙井";
+    }
+}
